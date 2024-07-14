2c03fe4e0d175c4a4992e9867a80cba956a8aab5
Ankitb3 <bankarankit3@gmail.com>
2024-07-14 23:25:25 +0530

    Add Typing animation and Loader

---
+++ src/components/TypingAnimation.tsx
@@ -0,0 +1,15 @@
+import TypingAnimation from "../components/magicui/typing-animation";
+interface Props{
+    text:string
+}
+const TextReveal=({text}:Props)=> {
+  return (
+    <TypingAnimation
+      className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-white dark:to-black"
+      text={text}
+      duration={500}
+
+    />
+  );
+}
+export default TextReveal;
